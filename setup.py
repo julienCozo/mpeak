a4b1a6a4e96925d482d50607cf5acbefcf39f270
Julien <julien.ortet@gmail.com>
2023-06-29 12:34:15 +0200

    add ssetup file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='mpeak',
+    version='0.1',
+    packages=['mpeak', 'application'],
+    url='https://github.com/julienCozo/mpeak',
+    license='GPL',
+    author='julien ortet',
+    author_email='julien.ortet@gmail.com',
+    description='Mountain peak API'
+)
